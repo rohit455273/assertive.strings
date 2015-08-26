63978a40eee53eae3ddf2e25d46949ab9a8b3b19
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-26 11:19:16 +0300

    added some missing tests

--- tests/testthat/test-is-empty-character.R
+++ tests/testthat/test-is-empty-character.R
@@ -27,36 +27,3 @@ test_that(
     )
   }
 )
-
-test_that("test.is_a_non_empty_string.empty_character.returns_false", {
-  expect_false(is_a_non_empty_string(character()))
-})
-
-test_that("test.is_a_non_empty_string.empty_string.returns_false", {
-  expect_false(is_a_non_empty_string(""))
-})
-
-test_that("test.is_a_non_empty_string.na_character_.returns_true", {
-  expect_true(is_a_non_empty_string(NA_character_))
-})
-
-test_that("test.is_a_non_empty_string.non_empty_string.returns_true", {
-  expect_true(is_a_non_empty_string("foo"))
-})
-
-test_that("test.is_an_empty_string.empty_character.returns_false", {
-  expect_false(is_an_empty_string(character()))
-})
-
-test_that("test.is_an_empty_string.empty_string.returns_true", {
-  expect_true(is_an_empty_string(""))
-})
-
-test_that("test.is_an_empty_string.na_character_.returns_false", {
-  expect_false(is_an_empty_string(NA_character_))
-})
-
-test_that("test.is_an_empty_string.non_empty_string.returns_false", {
-  expect_false(is_an_empty_string("foo"))
-})
-
